0f87f9425c133333c55357c447009e0d46652ada
Yuka Yamamoto <yamamoto_yuka@hotmail.com>
2022-08-03 20:48:48 -0700

    three-BufferGeometry-starfield

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,65 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild, OnInit } from '@angular/core';
+import * as THREE from 'three';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
-  title = 'starfild';
+export class AppComponent implements OnInit {
+  @ViewChild('container', { static: true })
+  container: ElementRef<HTMLDivElement>;
+  @ViewChild('myCanvas', { static: true }) myCanvas: ElementRef<HTMLDivElement>;
+
+  ngOnInit(): void {
+    let scene, camera, renderer;
+    let LINE_COUNT = 1000;
+    let geom = new THREE.BufferGeometry();
+    geom.setAttribute(
+      'position',
+      new THREE.BufferAttribute(new Float32Array(6 * LINE_COUNT), 3)
+    );
+    geom.setAttribute(
+      'velocity',
+      new THREE.BufferAttribute(new Float32Array(2 * LINE_COUNT), 1)
+    );
+    let pos = geom.getAttribute('position');
+    let pa = pos.array;
+    let vel = geom.getAttribute('velocity');
+    let va = vel.array;
+
+    let init = () => {
+      scene = new THREE.Scene();
+      camera = new THREE.PerspectiveCamera(
+        60,
+        this.container.nativeElement.clientWidth /
+          this.container.nativeElement.clientHeight,
+        1,
+        500
+      );
+      camera.position.z = 200;
+      renderer = new THREE.WebGLRenderer({
+        canvas: this.myCanvas.nativeElement,
+        antialias: true,
+      });
+      renderer.setSize(
+        this.container.nativeElement.clientWidth,
+        this.container.nativeElement.clientHeight
+      );
+
+
+      for(let line_index = 0; line_index < LINE_COUNT; line_index++){
+        let x = Math.random()*400 - 200;
+        let y = Math.random()*200 - 100;
+        let z = Math.random()*500 - 100;
+        let xx = x;
+        let zz = z;
+        // Line start
+        pa[6*line_index] = x;
+        pa[6*line_index+1] = y;
+        pa[6*line_index+2] = z;
+
+      }
+    };
+  }
 }
